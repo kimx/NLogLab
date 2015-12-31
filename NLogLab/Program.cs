bac1d742668ef07e9e670e36e775d1839dd83201
kimx <kimxinfo@gmail.com>
2015-12-31 15:23:12 +0800

    1

---
+++ NLogLab/Program.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config();
+            // Programing();
+        }
+
+        private static void Config()
+        {
+            var log = LogManager.GetCurrentClassLogger();
+            log.Debug("Debug");
+            log.Error("Error");
+        }
+
+        private static void Programing()
+        {
+            FileTarget target = new FileTarget();
+            target.Layout = "${longdate} ${logger} ${message}";
+            target.FileName = "${basedir}/KNLogs/${date:format=yyyyMMdd}.log";
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
+            var log = LogManager.GetLogger("log");
+            log.Debug("Kim Trace");
+        }
+    }
+}
